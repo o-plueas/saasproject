afc9286587179c3db8419e8aea49e5257374816f
ogochukwuugwu182608-rgb <ogochukwu.ugwu.182608@unn.edu.ng>
2026-03-31 13:29:03 -0700

    updated start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ echo "Starting Celery beat..."
 celery -A saasproject beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 echo "Starting Daphne..."
-exec daphne -b 0.0.0.0 -p 10000 saasproject.asgi:application
+exec daphne -b 0.0.0.0 -p ${PORT:-10000} saasproject.asgi:application
