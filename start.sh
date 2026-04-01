53318ed4b27b9f526feee34d3d29fe19d68e02a0
ogochukwuugwu182608-rgb <ogochukwu.ugwu.182608@unn.edu.ng>
2026-03-31 17:58:36 -0700

    Updated version of start

--- start.sh
+++ start.sh
@@ -1,14 +1,9 @@
+
 #!/bin/bash
 set -e
 
 echo "Running migrations..."
 python manage.py migrate --noinput
-
-echo "Starting Celery worker..."
-celery -A saasproject worker --uid=nobody -l info &
-
-echo "Starting Celery worker..."
-celery -A saasproject worker --concurrency=1 --uid=nobody -l info &
 
 echo "Starting Daphne..."
 exec daphne -b 0.0.0.0 -p ${PORT:-10000} saasproject.asgi:application
