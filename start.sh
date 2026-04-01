d05a9974806d41f5b54a56345d1a72464bd8e05f
ogochukwuugwu182608-rgb <ogochukwu.ugwu.182608@unn.edu.ng>
2026-04-01 10:30:47 -0700

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
-
 #!/bin/bash
 set -e
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+python manage.py migrate --noinput 2>&1
+echo "Migration done"
 
 echo "Starting Daphne..."
 exec daphne -b 0.0.0.0 -p ${PORT:-10000} saasproject.asgi:application
