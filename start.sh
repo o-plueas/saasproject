cc1002d45eaa7a436f170450d391eab8feeb6bc6
ogochukwuugwu182608-rgb <ogochukwu.ugwu.182608@unn.edu.ng>
2026-04-02 01:23:07 -0700

    start.sh update for cloudinary image fix

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ echo "Running migrations..."
 python manage.py migrate --noinput 2>&1
 echo "Migration done"
 
+echo "Checking storage backend..."
+python manage.py shell -c "from django.core.files.storage import default_storage; print('STORAGE:', default_storage.__class__)"
+
 echo "Creating superuser..."
 python manage.py createsuperuser --noinput || echo "Superuser already exists"
 
